a68c1da9f054aabdcdea553e0cdd2769dbf444c0
Meherul Meher <mehrul.islam@vivatelecoms.com>
2024-12-19 12:03:20 +0600

    mobile menu click solved

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 const mobileMenuButton = document.getElementById("mobile-menu-button");
 const mobileMenu = document.getElementById("mobile-menu");
+const mainMenu = document.getElementById("mainMenu");
 
 mobileMenuButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+
+mainMenu.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
+// links2.addEventListener("click", () => {
+//   mobileMenu.classList.toggle("hidden");
+// });
 
 // slide============================
 
